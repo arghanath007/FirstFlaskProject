5e01bbcb8454faca30706380620dbe5b330f37f7
arghanath <arghanath007@gmail.com>
2021-09-21 17:30:47 +0530

    Make a change in the config

--- market/__init__.py
+++ market/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///market.db'
 app.config['SECRET_KEY']='0e4c7ff554460dfc9e4e0a20a3b2ef'
+app.config['DEBUG']=True
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager=LoginManager(app)
--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ from market import app
 
 application=app
 #checks if the run.py file has executed directly and not imported
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
